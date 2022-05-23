aab88ae8274fcd483a8a7c7c2908cc5fbe2b4c70
rorybryett <rory.bryett@dwp.gov.uk>
2022-05-23 20:07:06 +0100

    ci pipelines, terrafom etc

---
+++ hbase-export.tf
@@ -0,0 +1,10 @@
+data "local_file" "hbase_snapshot_exporter_script" {
+  filename = "files/hbase-snapshot-exporter.sh"
+}
+
+resource "aws_s3_bucket_object" "htme_wrapper_script" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "component/hbase-exporter/hbase-snapshot-exporter.sh"
+  content    = data.local_file.hbase_snapshot_exporter_script.content
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+}
