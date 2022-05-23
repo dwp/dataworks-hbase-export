60e39795ac22caefbdf346088cc5129ac1792adf
rorybryett <rory.bryett@dwp.gov.uk>
2022-05-23 16:04:15 +0100

    DW-8709 initial commit of hbase snapshot export script

--- hbase-snapshot-exporter/hbase-snapshot-exporter.sh
+++ hbase-snapshot-exporter/hbase-snapshot-exporter.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-DATE=`date +"%Y%m%d"`
 TABLE_NAME=$1
 EXPORT_LOCATION=$2
 
+DATE=`date +"%Y%m%d"`
+VALID_SNAPSHOT_NAME_REGEX='^-.a-zA-Z_0-9'
+
 function sanitise_table_name() {
-    TABLE_NAME=$1
     # replace spaces with underscores
     SANITISED_TABLE_NAME=${TABLE_NAME// /_}
     # replace colons with fullstops
-    SANITISED_TABLE_NAME=${TABLE_NAME//:/.}
+    SANITISED_TABLE_NAME=${SANITISED_TABLE_NAME//:/.}
     # clean out anything that's not valid
-    SANITISED_TABLE_NAME=${SANITISED_TABLE_NAME//[^.a-zA-Z0-9_]/}
+    SANITISED_TABLE_NAME=${SANITISED_TABLE_NAME//[$VALID_SNAPSHOT_NAME_REGEX]/}
     # lowercase
     SANITISED_TABLE_NAME=`echo -n $SANITISED_TABLE_NAME | tr A-Z a-z`
     # appenddate
@@ -21,20 +22,43 @@ function sanitise_table_name() {
 
 function valid_snapshot_name() {
     SNAPSHOT_NAME=$1
-    regex='^-.a-zA-Z_0-9'
-    if ! [[ "$SNAPSHOT_Name" =~ [$regex] ]]; then
+    if ! [[ "$SNAPSHOT_NAME" =~ [$VALID_SNAPSHOT_NAME_REGEX] ]]; then
         return 0
     else
         return 1
     fi
 }
 
+function get_snapshot() {
+    # Verify snapshot was created (retry logic maybe required)
+    hbase snapshot info -list-snapshots | grep ${SNAPSHOT_NAME}
+    if [ $? != 0 ]; then
+        echo "could not find snapshot with name ${SNAPSHOT_NAME}"
+        return 1
+    else
+        return 0
+    fi
+}
+
+function take_snapshot() {
+    if get_snapshot; then
+        echo "snapshot with name ${SNAPSHOT_NAME} already exists. exiting..."
+        exit 1
+    fi
+
+    echo "taking snapshot of table ${TABLE_NAME}"
+    hbase snapshot create -n ${SNAPSHOT_NAME} -t ${TABLE_NAME}
+    if [ $? != 0 ]; then
+        echo "snapshot creation failed. exiting..."
+        exit 1
+    fi
+}
+
 function export_snapshot() {
-    EXPORT_LOCATION=$1
     echo "exporting snapshot ${SNAPSHOT_NAME} to ${EXPORT_LOCATION}"
     hbase snapshot export -snapshot ${SNAPSHOT_NAME} -copy-to $EXPORT_LOCATION
     if [ $? != 0 ]; then
-        echo "export failed"
+        echo "export failed. exiting..."
         exit 1
     else
         echo "export finished"
@@ -42,7 +66,7 @@ function export_snapshot() {
 }
 
 if [ -z $SNAPSHOT_NAME ]; then
-    echo "No snapsnot name provided. Sanitising table name"
+    echo "No snapsnot name provided. Sanitising table name ${TABLE_NAME}"
     SNAPSHOT_NAME=$(sanitise_table_name $TABLE_NAME)
     echo "New snapshot name will be: ${SNAPSHOT_NAME}"
 fi
@@ -51,6 +75,5 @@ if ! valid_snapshot_name $SNAPSHOT_NAME; then
     echo "invalid snapshot name: ${SNAPSHOT_NAME}"
 fi
 
-hbase snapshot create -n ${SNAPSHOT_NAME} -t ${TABLE_NAME}
-hbase snapshot info -list-snapshots | grep ${SNAPSHOT_NAME}
-export_snapshot $EXPORT_LOCATION
+take_snapshot
+export_snapshot
