bd6266173bab267611acc4776c98b5627e7009d4
rorybryett <rory.bryett@dwp.gov.uk>
2022-05-25 08:06:05 +0100

    output location of script in S3

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,7 @@ output "hbase_export_bucket" {
     arn = aws_s3_bucket.hbase_export_bucket.arn
   }
 }
+
+output "hbase_snapshot_exporter_script" {
+  value = "s3://${data.terraform_remote_state.common.outputs.config_bucket.id}/${aws_s3_bucket_object.hbase_snapshot_exporter_script.key}"
+}
