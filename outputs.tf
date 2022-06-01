aa191d6f5cca6caccd051d1dd3efb32a544344f2
rbryett <13303701+rbryett@users.noreply.github.com>
2022-06-01 09:05:20 +0100

    output arn of the kms key used to encrypt the export bucket at rest (#7)
    Co-authored-by: rorybryett <rory.bryett@dwp.gov.uk>

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,8 @@
 output "hbase_export_bucket" {
   value = {
-    id  = aws_s3_bucket.hbase_export_bucket.id
-    arn = aws_s3_bucket.hbase_export_bucket.arn
+    id      = aws_s3_bucket.hbase_export_bucket.id
+    arn     = aws_s3_bucket.hbase_export_bucket.arn
+    key_arn = aws_kms_key.hbase_export_s3.arn
   }
 }
 
