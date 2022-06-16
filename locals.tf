b65d09e061d100b0a4eae2704e75ee3de0a159f5
rorybryett <rory.bryett@dwp.gov.uk>
2022-06-16 14:14:36 +0100

    Revert "DW-8795 kms key policy allows HBASE Ingest EMR clusters (instance profile) to decrypt S3 objects in hbase export bucket for importing (#9)"
    This reverts commit 33d9c957cfeb170e7c4194f6f2a607626f15cda6.

--- locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-    accounts = {
-        development    = "475593055014"
-        qa             = "003710426629"
-        integration    = "337387380515"
-        preprod        = "445364893246"
-        production     = "517406633788"
-    }
-}
